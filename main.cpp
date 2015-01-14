0ff3c55e5eb8c16fa0ada0bad874bec2437ff52d
Dmitro Shestakov <altais@ukr.net>
2015-01-14 23:58:41 +0200

    v0.1 Beta ready.

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ constexpr uint64_t F_CPU = 54000000ULL;
 #include "usart.h"
 
 using namespace Mcucpp;
-using Usart = Usarts::UsartIrq<USART1_BASE, Usarts::RemapUsart1_Pa9Pa10, 128, 16>;
+using Usart = Usarts::UsartIrq<USART1_BASE, Usarts::RemapUsart1_Pa9Pa10, 512, 16>;
 DECLAREIRQ(Usart, USART1)
 
 void ClockInit()
@@ -80,15 +80,18 @@ void ClockInit()
 		 configuration. User can add here some code to deal with this error */
 	}
 }
-constexpr uint8_t channelsQuantity = 4;
+constexpr uint8_t channelsQuantity = 8;
+constexpr uint8_t samplesQuantity = 8;
+constexpr uint32_t maxSamplingF = 16384;
 static uint8_t channels = 2;
 
 constexpr uint16_t GetDivider(uint8_t nf)
 {
-	return nf == 8 ? 2 : 1 << ((8 - nf) << 1);
+	return nf < 8 ? 2 << ((8 - nf) * 2) :
+			nf == 8 ? 5 : 3;
 }
 
-static uint16_t adcSamplesArr[2][8][channelsQuantity];		//buf x nsample x nchannel
+static uint16_t adcSamplesArr[2][samplesQuantity][channelsQuantity];		//buf x nsample x nchannel
 
 extern "C"
 {
@@ -111,14 +114,13 @@ extern "C"
 		else goto END;
 		for(uint8_t c = 0; c < channels; ++c)
 		{
-			for(uint8_t s = 0; s < 8; ++s)
+			for(uint8_t s = 0; s < samplesQuantity; ++s)
 			{
-				adcSamples[c] += (*(uint16_t(*)[2][8][channels])adcSamplesArr)[nbuf][s][c];
+				adcSamples[c] += (*(uint16_t(*)[2][samplesQuantity][channels])adcSamplesArr)[nbuf][s][c];
 			}
-				Usart::NewLine();
-				Usart::Puts<DataFormat::Dec>(adcSamples[c] / 8);
-				adcSamples[c] = 0;
-				if(c < channels - 1) Usart::Putch(';');
+			Usart::Puts<DataFormat::Dec>(adcSamples[c] / samplesQuantity);
+			adcSamples[c] = 0;
+			if(c < channels - 1) Usart::Putch(';');
 		}
 		Usart::Putch('\r');
 	END:
@@ -139,9 +141,9 @@ int main()
 	Usart::Init<Usarts::DefaultCfg, Usarts::BaudRate<921600UL>>();
 	{
 		using namespace Timers;
-		Tim3::Init<ARRbuffered | UpCount, F_CPU / (16384 * 8), 2>();
+		Tim3::Init<ARRbuffered | UpCount, 1 + F_CPU / (maxSamplingF * samplesQuantity * 2), GetDivider(1)>();
 		Tim3::MasterModeSelect(MM_Update);
-		Tim3::Enable();
+//		Tim3::Enable();
 	}{
 		using namespace Adcs;
 		Adc::Init<Single | DownScan,
@@ -159,23 +161,65 @@ int main()
 
 	uint32_t channelsMask = Populate(channels) << (channelsQuantity - channels);
 	Adc::SelectChannels(channelsMask);
-	Adc::Dma::SetCounter(channels * 16);		//8 samples, double buffering
+	Adc::Dma::SetCounter(channels * samplesQuantity * 2);		//8 samples, double buffering
 	Adc::Dma::Enable();
 	Adc::Start();
-	Usart::Puts("\r\nAnalog Data Logger v1.0\r\n");
-	//TODO: Enable watchdog
+	Usart::Puts("\r\nAnalog Data Logger v1.0\r\nPress Backspace button\r\n");
+	delay_ms<2000>();
+	//TODO: Enable watchdog, save settings to NV memory
+	uint8_t c;
 	while(true)
 	{
-		delay_us<40>();
-	}
-	uint8_t* str;
-	while(true)
-	{
-		while(true)
+		if(Usart::Getch(c) && c == '\b')
 		{
-			Usart::Puts("\r\nEnter Min voltage in mV (in range 500...4200mV): ");
-			while(!(str = Usarts::GetStr<Usart>()))
-				;
+			Tim3::Disable();
+			Adc::Dma::Disable();
+			while(true)
+			{
+				Usart::Puts("\r \r\nEnter number of active channels [1-8]: ");
+				while(!Usart::Getch(c))
+					;
+				c -= 0x30;
+				if(0 < c && c < 9)
+				{
+					channels = c;
+					Adc::Dma::SetCounter(channels * samplesQuantity * 2);		//8 samples, double buffering
+					uint32_t channelsMask = Populate(channels) << (channelsQuantity - channels);
+					Adc::SelectChannels(channelsMask);
+					Usart::Puts("\r\nOk.");
+					break;
+				}
+				else Usart::Puts("\r\nInvalid input.");
+			}
+			while(true)
+			{
+				Usart::Puts("\r\nEnter sampling frequency [1-9]."
+							"\r\nPositions 7-9 available depending on selected number of channels"
+							"\r\n1 - 1 Hz"
+							"\r\n2 - 4 Hz"
+							"\r\n3 - 16 Hz"
+							"\r\n4 - 64 Hz"
+							"\r\n5 - 256 Hz"
+							"\r\n6 - 1 kHz");
+				if(channels < 4) Usart::Puts("\r\n7 - 4 kHz");
+				if(channels < 3) Usart::Puts("\r\n8 - 6.55 kHz");
+				if(channels == 1) Usart::Puts("\r\n9 - 10.92 kHz");
+				Usart::Puts("\n: ");
+				while(!Usart::Getch(c))
+					;
+				c -= 0x30;
+				if(0 < c && c < (channels == 1 ? 10 :
+								 channels < 3 ? 9 :
+								 channels < 4 ? 8 : 7))
+				{
+					Tim3::WriteAutoReload(GetDivider(c));
+					Usart::Puts("\r\nOk.\r\n");
+					break;
+				}
+				else Usart::Puts("\r\nInvalid input.");
+			}
+			Adc::Dma::Enable();
+			Tim3::Enable();
 		}
 	}
 }
